b896557192738dd1fa409301f74595a643695838
sitaramgarlapati <51022447+sitaramgarlapati@users.noreply.github.com>
2023-06-22 09:08:53 +0530

    added dropdown reads and json binding

--- src/App.js
+++ src/App.js
@@ -4,19 +4,40 @@ import React from 'react';
 import { FormGroup, FormLabel, FormControl  } from 'react-bootstrap';
 
 
-function App() {
-  
-  const [selectedOption, setSelectedOption] = React.useState('');
 
-  const handleChange = (event) => {
-    setSelectedOption(event.target.value);
+
+  
+
+  const options = [
+    { id: 1, label: 'Option 1' },
+    { id: 2, label: 'Option 2' },
+    { id: 3, label: 'Option 3' },
+  ];
+  
+
+  const App = () => {
+
+  
+    // State to store the selected option
+    const [selectedOptionica, setSelectedOptionica] = React.useState('');
+    const [selectedOptionproduct, setSelectedOptionproduct] = React.useState('');
+
+ // Event handler for option selection
+ const handleChange = (event) => {
+  setSelectedOptionproduct(event.target.value);
+};
+    
+
+  // Event handler for option selection
+  const handleOptionChange = (event) => {
+    setSelectedOptionica(event.target.value);
   };
 
-  return (
+    return (
      <form>
       <FormGroup controlId="formName">
         <FormLabel>Name</FormLabel>
-        <FormControl type="text" placeholder="Enter name" value={selectedOption} /> 
+        <FormControl type="text" placeholder="Enter name" value={selectedOptionproduct} /> 
       </FormGroup>
       <FormGroup controlId="formEmail">
         <FormLabel>Email</FormLabel>
@@ -31,7 +52,7 @@ function App() {
         <FormLabel>Select Type</FormLabel>
         <FormControl
   as="select"
-  value={selectedOption}
+  value={selectedOptionproduct}
   onChange={handleChange}
   aria-label="Select an option"
 >
@@ -40,8 +61,21 @@ function App() {
   <option>Option 3</option>
 </FormControl>
       </FormGroup>
+
+      <div>
+      <select value={selectedOptionica} onChange={handleOptionChange}>
+        <option value="">Select an option</option>
+        {options.map((option) => (
+          <option key={option.id} value={option.id}>
+            {option.label}
+          </option>
+        ))}
+      </select>
+      <p>Selected option: {selectedOptionica}</p>
+    </div>
+
     </form>
   );
-}
+};
 
 export default App;
